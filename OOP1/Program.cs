87ddb1b15c16df7fcce7fde11b397f88c27418b2
HsynOzcn <59474596+Ozc9n@users.noreply.github.com>
2021-01-22 11:17:12 +0300

    Oop introduction with C#

---
+++ OOP1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            product.CategoriyId = 2;
+            product.ProductName = "Masa";
+            product.UnitPrice = 500;
+            product.UnitInStock = 3;
+
+            //Diger gösterim 
+            Product product2= new Product { Id=2,CategoriyId=5,ProductName=
+            "Kalem",UnitPrice=15,UnitInStock=50};
+
+            //Pascal Case   //Camel Case 
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+            Console.WriteLine(product.ProductName);
+
+            /*
+            int sayi = 100;
+            productManager.DoSomething(sayi);
+            Console.WriteLine(sayi);
+            */
+            // int double bool değer tipler
+            // diziler,class ,abstract class, interface referans tip
+
+        }
+    }
+}
