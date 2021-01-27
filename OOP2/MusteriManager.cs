66fbc70e43f4bcdf08c6b555866f5d10e8338ba3
HsynOzcn <59474596+Ozc9n@users.noreply.github.com>
2021-01-27 19:40:46 +0300

    SOLID prensibi L kuralı örneği

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager
+    {
+        // İş sınıfları 
+        public void Add(Musteri musteri)
+        {
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri = new GercekMusteri();
+            musteri.Adi = "Hüseyin";
+            musteri.SoyAdi = "Özcan";
+            musteri.TcNo = "12312312312";
+            musteri.Id = 1;
+            musteri.MusteriNo = "1";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 1;
+            musteri2.MusteriNo = "1";
+            musteri2.VergiNo = "123";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new GercekMusteri();
+
+            MusteriManager manager = new MusteriManager();
+            manager.Add(musteri2);
+
+        }
+    }
+}
