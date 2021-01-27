13823a048406bb4582856fbc2ff9d68d46513be6
HsynOzcn <59474596+Ozc9n@users.noreply.github.com>
2021-01-27 19:42:55 +0300

    Solid prensibi O maddesi ve Interface operasyonu

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager,ILoggerService loggerService)
+        {
+
+            // Çok önemli bir nokta
+            krediManager.Hesapla();
+            // dependincies injection 
+            loggerService.Log();
+            // Bütün hesaplamalar taşıt kredisi üzerinden yapılır.new yapmaktan çekin !!!
+            //TasitKrediManager kredi1 = new TasitKrediManager();
+            //kredi1.Hesapla();
+        }
+        public void KrediBilgilendir(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IKrediManager tasitKredi = new TasitKrediManager();
+            IKrediManager ihtiyacKredi = new IhtiyacKrediManager();
+            IKrediManager konutKredi = new KonutKrediManager();
+            //kredi1.Hesapla();
+
+            ILoggerService smsLoggerService = new SmsLoggerService();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            //basvuruManager.BasvuruYap(tasitKredi,new SmsLoggerService());
+            //basvuruManager.BasvuruYap(ihtiyacKredi,new DatabaseLoggerService());
+            //basvuruManager.BasvuruYap(konutKredi,new SmsLoggerService());
+            // --------------------------------------------------------
+            basvuruManager.BasvuruYap(tasitKredi,smsLoggerService);
+            basvuruManager.BasvuruYap(ihtiyacKredi,databaseLoggerService);
+            basvuruManager.BasvuruYap(konutKredi,fileLoggerService);
+            // SOLID prensipinin O harfi güncellemelerin kodları etkilememesi
+            basvuruManager.BasvuruYap(new EsnafKrediManager(), smsLoggerService);
+
+
+
+            // !!!İnterfaceleri birbirinin alternatifi fakat içeriği farklı olan operasyonlar için kullanılır...!!!
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {tasitKredi,ihtiyacKredi,konutKredi};
+            //basvuruManager.KrediBilgilendir(krediler);
+
+
+        }
+    }
+}
