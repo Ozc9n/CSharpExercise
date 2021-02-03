e6cc606d56ac3c300dd6d13eb83272ec8fb4314b
HsynOzcn <59474596+Ozc9n@users.noreply.github.com>
2021-02-03 19:01:31 +0300

    GameProject similasyonu

---
+++ GameProject/GameProject/UserValidationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1998 && gamer.FirstName == "İrfan" && gamer.LastName == "Kahveci" && gamer.IdentyNumber == 123412)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
